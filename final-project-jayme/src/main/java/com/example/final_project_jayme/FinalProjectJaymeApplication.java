3dcb9960ea25706a60bda68099e476551cfc2cb6
johnn-vozzi-wmw <johnn.bryan@wmw.com.br>
2024-07-05 20:15:39 -0300

    add essential files

---
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/FinalProjectJaymeApplication.java
@@ -0,0 +1,15 @@
+package com.example.final_project_jayme;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FinalProjectJaymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectJaymeApplication.class, args);
+	}
+
+}
