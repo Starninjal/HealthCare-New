8d301e48aac0f4f0febf45cfa00cb362ef048c01
johnn-vozzi-wmw <johnn.bryan@wmw.com.br>
2024-07-05 20:32:08 -0300

    alter ProcedimentoService

--- final-project-jayme/src/main/java/com/example/final_project_jayme/service/ProcedimentoService.java
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/service/ProcedimentoService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.final_project_jayme.dto.ProcedimentoDTO;
 import com.example.final_project_jayme.entities.Procedimento;
+import com.example.final_project_jayme.exception.CustomException;
 import com.example.final_project_jayme.repositories.ProcedimentoRepository;
 
 @Service
@@ -15,8 +17,8 @@ public class ProcedimentoService {
     @Autowired
     private ProcedimentoRepository procedimentoRepository;
 
-    
-    public Procedimento saveProcedimento(ProcedimentoDTO procedimentoDTO) {
+
+    public Procedimento save(ProcedimentoDTO procedimentoDTO) {
         Procedimento procedimento = new Procedimento();
         BeanUtils.copyProperties(procedimento, procedimento);
         return procedimentoRepository.save(procedimento);
@@ -24,5 +26,17 @@ public class ProcedimentoService {
 
     public List<Procedimento> findAllProcedimentos() {
         return procedimentoRepository.findAll();
+    }
+
+    public Procedimento findProcedimentoBy(Long procedimentoId) {
+        return findAllProcedimentos().stream().
+                        filter(p -> p.getId() == procedimentoId)
+                        .findFirst().orElseThrow(() -> new CustomException("Procedimento não encontrado"));
+    }
+
+    public Procedimento deleteProcedimentoBy(Long procedimentoId) {
+        Procedimento procedimento = findProcedimentoBy(procedimentoId);
+        procedimentoRepository.delete(procedimento);
+        return procedimento;
     }
 }
