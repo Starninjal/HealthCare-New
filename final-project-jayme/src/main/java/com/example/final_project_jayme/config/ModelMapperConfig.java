7f3bca63ce62fb7330a807a243bcba7899cdc75a
johnn-vozzi-wmw <johnn.bryan@wmw.com.br>
2024-07-05 20:15:15 -0300

    add config

---
+++ final-project-jayme/src/main/java/com/example/final_project_jayme/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.example.final_project_jayme.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
